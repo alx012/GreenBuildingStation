6db217c9298e1b52223ada12c4f40c85a94a3368
ckhsc17 <94750792+Bowenbowenbowenbowen@users.noreply.github.com>
2025-05-17 15:40:19 +0800

    Test embedded page 4 test speed

--- urbanclimate-new.php
+++ urbanclimate-new.php
@@ -610,17 +610,17 @@ if (session_status() == PHP_SESSION_NONE) {
                         <button class="button" onclick="hideLoadDialog()" style="margin-left: 10px; background-color: #999;"><?php echo __('cancel_btn'); ?></button>
                     </div>
                 </div>
-                
+                <!--
                 <div id="osmMapContainer" style="display:none;">
                     <h3>建築物高度分析地圖</h3>
                     <div id="map" style="width:100%; height:1200;"></div>
                     <div class="info" id="info">請畫出範圍來查詢建築物高度</div>
-                </div>
-                   <!-- 
+                </div>-->
+                    
                 <div class="map-section">
                     <h3>建築物高度分析地圖</h3>
                     <iframe src="overpass.html" width="100%" height="800" frameborder="0"></iframe>
-                </div>-->
+                </div>
 
                 <div class="canvas-container">
                     <canvas id="drawingCanvas" width="1500" height="800"></canvas>
